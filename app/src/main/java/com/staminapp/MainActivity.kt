1da19dafed8734f7062a75e05fb1f0a838380a7e
NehuenLlanos <nehuengllanos@gmail.com>
2022-11-09 17:15:08 -0300

    Texfields and button created. Theme partially done. We need to discuss the theme colors.

--- app/src/main/java/com/staminapp/MainActivity.kt
+++ app/src/main/java/com/staminapp/MainActivity.kt
@@ -3,31 +3,96 @@ package com.staminapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.staminapp.ui.theme.StaminappAppTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StaminappAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                SignInCard()
             }
         }
     }
 }
+
+@Composable
+fun SignInCard() {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+        Column(modifier = Modifier.padding(24.dp)) {
+            UsernameTextField()
+            PasswordTextField()
+            SignInButton()
+        }
+    }
+}
+
+@Composable
+fun UsernameTextField() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    Surface(modifier = Modifier.padding(24.dp)) {
+        TextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            label = { Text(text = "Email") },
+        )
+    }
+}
+
+@Composable
+fun PasswordTextField() {
+    var password by remember { mutableStateOf(TextFieldValue("")) }
+    Surface(modifier = Modifier.padding(24.dp)) {
+        TextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = { Text(text = "Contrasenia") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
+    }
+}
+
+@Composable
+fun SignInButton() {
+    Surface(modifier = Modifier.padding(24.dp)) {
+        Button(
+            onClick = {
+            //your onclick code
+            },
+            colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.primary),
+            shape = RoundedCornerShape(50.dp)) {
+            Text(text = "Ingresar",color = MaterialTheme.colors.background)
+
+        }
+    }
+}
+
+
 
 @Composable
 fun Greeting(name: String) {
@@ -38,6 +103,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     StaminappAppTheme {
-        Greeting("Android")
+        SignInCard()
     }
 }
