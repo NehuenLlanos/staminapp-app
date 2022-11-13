83a878b4d64ad48071a1983cc5ef7d198a37cd7e
NehuenLlanos <nehuengllanos@gmail.com>
2022-11-12 23:52:44 -0300

    Fix some problems with Top Part of profile. Colors in TextFields, TopBar and BottomBar missing in Explore and Profile.

--- app/src/main/java/com/staminapp/ProfileScreen.kt
+++ app/src/main/java/com/staminapp/ProfileScreen.kt
@@ -9,6 +9,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -16,10 +17,14 @@ import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.asImageBitmap
@@ -31,6 +36,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -42,54 +48,31 @@ import androidx.compose.ui.unit.sp
 fun ProfileScreen() {
     Column(
         modifier = Modifier
-            .fillMaxWidth(0.5f)
+            .fillMaxSize()
             .verticalScroll(rememberScrollState())
-            .background(MaterialTheme.colors.background),
+            .background(MaterialTheme.colors.background)
+            .padding(16.dp)
     ) {
         Row(modifier = Modifier
-            .wrapContentWidth(Alignment.Start)
-            .fillMaxWidth(1f)) {
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
+        ) {
             UserImage()
-            UserNameTitle(Modifier.padding(10.dp))
+            UserNameTitle()
         }
-        Row(modifier = Modifier
-            .align(Alignment.CenterHorizontally)
-            .fillMaxWidth(1f)
-            .padding(10.dp)) {
-            StatisticsButton(Modifier.width(150.dp)
-                .padding(10.dp)
-                .weight(1f))
+        StatisticsButton(Modifier.padding(vertical = 16.dp))
+        PersonalInfoText()
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(top = 12.dp, bottom = 64.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically)
+        ) {
+            GenderTextField(Modifier.fillMaxWidth())
+            EMailTextField(Modifier.fillMaxWidth())
+            BirthdateTextField(Modifier.fillMaxWidth())
         }
-        Row(modifier = Modifier
-            .fillMaxWidth(1f)
-            .padding(10.dp)) {
-            PersonalInfoText()
-
-        }
-        Row(modifier = Modifier
-            .fillMaxWidth(1f)
-            .padding(10.dp)) {
-            GenderTextField(Modifier.weight(1f))
-        }
-        Row(modifier = Modifier
-            .fillMaxWidth(1f)
-            .padding(10.dp)) {
-            EMailTextField(Modifier.weight(1f))
-        }
-        Row(modifier = Modifier
-            .fillMaxWidth(1f)
-            .padding(10.dp)) {
-            BirthdateTextField(Modifier.weight(1f))
-        }
-        Row(modifier = Modifier
-            .fillMaxWidth(1f)
-            .padding(10.dp)
-            .align(Alignment.CenterHorizontally)) {
-            SignOutButton(
-                Modifier
-                    .weight(1f)
-                    .width(200.dp))
-        }
+        SignOutButton()
     }
 }
 
@@ -115,7 +98,10 @@ fun UserImage(){
         .clip(CircleShape)
         .fillMaxHeight()
         .fillMaxWidth()
-        .clickable { loadImage.launch("image/*") }, contentScale = ContentScale.FillWidth)                       // con FillBounds se soluciona pero queda muy feo
+        .rotate(90f)
+        .clickable { loadImage.launch("image/*") },
+        contentScale = ContentScale.Crop)
+        // con FillBounds se soluciona pero queda muy feo
 
 //    OutlinedButton(onClick = {loadImage.launch("image/*")}, modifier = Modifier.width(150.dp).padding(10.dp)) {
 //        Text(text="Cargar Imagen")
@@ -123,10 +109,156 @@ fun UserImage(){
 }
 
 @Composable
-fun UserNameTitle(modifier: Modifier){
-    Text(modifier = modifier, text = buildAnnotatedString { withStyle(style= SpanStyle(color = MaterialTheme.colors.primaryVariant, fontSize = 40.sp, fontWeight = FontWeight.Bold)){append("DWAYNE JOHNSON")}
-        withStyle(style= SpanStyle(color = MaterialTheme.colors.primaryVariant, fontSize = 30.sp)){append("50 años")}
-    })
+fun UserNameTitle(){
+    Column(
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Text(text = "dwayne johnson".uppercase(),
+            color = MaterialTheme.colors.primaryVariant,
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 2
+        )
+        Text(text = "50 años".uppercase(),
+            color = MaterialTheme.colors.primaryVariant,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1
+        )
+    }
+
+//    Text(text = buildAnnotatedString { withStyle(style= SpanStyle(color = MaterialTheme.colors.primaryVariant,
+//                                    fontSize = 24.sp, fontWeight = FontWeight.Bold)){append("DWAYNE JOHNSON")}
+//
+//        withStyle(style= SpanStyle(color = MaterialTheme.colors.primaryVariant, fontSize = 16.sp)){append("50 años")}
+//    })
+}
+
+@Composable
+fun StatisticsButton(modifier: Modifier){
+    OutlinedButton(
+        modifier = modifier.fillMaxWidth(),
+        onClick = {},
+        shape = RoundedCornerShape(50), // = 50% percent
+        // or shape = CircleShape
+    ) {
+        Text(text="Ver estadísticas")
+    }
+}
+
+@Composable
+fun PersonalInfoText(){
+    Text(text = "Información Personal",
+        color = MaterialTheme.colors.primaryVariant,
+        fontSize = 28.sp,
+        fontWeight = FontWeight.Bold,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 1
+    )
+//    Text(buildAnnotatedString { withStyle(style= SpanStyle(color = MaterialTheme.colors.primaryVariant, fontWeight = FontWeight.Bold, fontSize = 30.sp)){append("Información Personal")}})
+}
+
+
+@Composable
+fun GenderTextField(modifier: Modifier){
+    var expanded by remember { mutableStateOf(false) }
+    var genre by remember { mutableStateOf(TextFieldValue("Género")) }
+    Box(modifier = modifier
+        .wrapContentSize(Alignment.TopStart)) {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = genre,
+            readOnly = true,
+            onValueChange = {
+                genre = it
+            },
+            label = { Text(text = "Género", fontWeight = FontWeight.Bold) },
+            textStyle = TextStyle(
+                MaterialTheme.colors.primaryVariant,
+                fontWeight = FontWeight.Bold
+            ),
+            trailingIcon = {
+                Icon(
+                    Icons.Filled.ExpandMore ,
+                    contentDescription = null,
+                    Modifier.clickable {
+                        expanded = true
+                    }
+                )
+            }
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(onClick = {
+                genre = TextFieldValue("Masculino")
+                }) {
+                Text("Masculino")
+            }
+            DropdownMenuItem(onClick = {
+                genre = TextFieldValue("Femenino")
+                }) {
+                Text("Femenino")
+            }
+            DropdownMenuItem(onClick = {
+                genre = TextFieldValue("Otro")
+                }) {
+                Text("Otro")
+            }
+        }
+    }
+}
+
+@Composable
+fun EMailTextField(modifier: Modifier){
+    var email by remember { mutableStateOf(TextFieldValue("therock@gmail.com")) }
+    TextField(
+        modifier = modifier,
+        value = email,
+        readOnly = true,
+        onValueChange = {
+            email = it
+        },
+        label = { Text(text = "Email", fontWeight = FontWeight.Bold) },
+        textStyle = TextStyle(
+            MaterialTheme.colors.primaryVariant,
+            fontWeight = FontWeight.Bold
+        )
+    )
+}
+
+@Composable
+fun BirthdateTextField(modifier: Modifier){
+    var birthdate by remember { mutableStateOf(TextFieldValue("02/02/2022")) }
+    TextField(
+        modifier = modifier,
+        value = birthdate,
+        readOnly = true,
+        onValueChange = {
+            birthdate = it
+        },
+        label = { Text(text = "Fecha de nacimiento", fontWeight = FontWeight.Bold) },
+        textStyle = TextStyle(
+            MaterialTheme.colors.primaryVariant,
+            fontWeight = FontWeight.Bold
+        )
+    )
+}
+
+@Composable
+fun SignOutButton(){
+    OutlinedButton(
+        modifier = Modifier.fillMaxWidth(),
+        onClick = {},
+        shape = RoundedCornerShape(50), // = 50% percent
+        colors = ButtonDefaults.buttonColors(Red),
+        // or shape = CircleShape
+    ) {
+        Text(text="Cerrar sesión")
+    }
 }
 
 //@Composable
@@ -162,83 +294,3 @@ fun UserNameTitle(modifier: Modifier){
 //        ),
 //    )
 //}
-
-@Composable
-fun GenderTextField(modifier: Modifier){
-    var gender by remember { mutableStateOf(TextFieldValue("")) }
-    TextField(
-        modifier = modifier,
-        value = gender,
-        onValueChange = {
-            gender = it
-        },
-        label = { Text(text = "Género") },
-        textStyle = TextStyle(
-            MaterialTheme.colors.primaryVariant,
-            fontWeight = FontWeight.Bold
-        ),
-    )
-}
-
-@Composable
-fun EMailTextField(modifier: Modifier){
-    var email by remember { mutableStateOf(TextFieldValue("")) }
-    TextField(
-        modifier = modifier,
-        value = email,
-        readOnly = true,
-        onValueChange = {
-            email = it
-        },
-        label = { Text(text = "Email") },
-        textStyle = TextStyle(
-            MaterialTheme.colors.primaryVariant,
-            fontWeight = FontWeight.Bold
-        ),
-    )
-}
-
-@Composable
-fun BirthdateTextField(modifier: Modifier){
-    var birthdate by remember { mutableStateOf(TextFieldValue("")) }
-    TextField(
-        modifier = modifier,
-        value = birthdate,
-        readOnly = true,
-        onValueChange = {
-            birthdate = it
-        },
-        label = { Text(text = "Fecha de nacimiento") },
-        textStyle = TextStyle(
-            MaterialTheme.colors.primaryVariant,
-            fontWeight = FontWeight.Bold
-        ),
-    )
-}
-
-@Composable
-fun StatisticsButton(modifier: Modifier){
-    OutlinedButton(onClick = {}, modifier = modifier,
-        shape = RoundedCornerShape(50), // = 50% percent
-        // or shape = CircleShape
-        ) {
-        Text(text="Ver estadísticas")
-    }
-}
-
-@Composable
-fun SignOutButton(modifier: Modifier){
-    OutlinedButton(onClick = {}, modifier = modifier,
-        shape = RoundedCornerShape(50), // = 50% percent
-        colors = ButtonDefaults.buttonColors(Red),
-        // or shape = CircleShape
-    ) {
-        Text(text="Cerrar sesión")
-    }
-}
-
-
-@Composable
-fun PersonalInfoText(){
-    Text(buildAnnotatedString { withStyle(style= SpanStyle(color = MaterialTheme.colors.primaryVariant, fontWeight = FontWeight.Bold, fontSize = 30.sp)){append("Información Personal")}})
-}
