5b8c3a653b489a039c843e14aeb9c202e7644c2f
Alejo Flores Lucey <alejo@misterflores.com>
2022-11-15 16:29:22 -0300

    Fixed problem that prevented compiling

--- app/src/main/java/com/staminapp/util/ComposeUtils.kt
+++ app/src/main/java/com/staminapp/util/ComposeUtils.kt
@@ -7,14 +7,6 @@ import androidx.compose.ui.platform.LocalSavedStateRegistryOwner
 import com.staminapp.MyApplication
 
 @Composable
-fun getViewModelFactory(defaultArgs: Bundle? = null): ViewModelFactory {
-    val application = (LocalContext.current.applicationContext as MyApplication)
-    val sessionManager = application.sessionManager
-    val userRepository = application.userRepository
-    return ViewModelFactory(sessionManager, userRepository, LocalSavedStateRegistryOwner.current, defaultArgs)
-}
-
-@Composable
 fun getRoutineViewModelFactory(defaultArgs: Bundle? = null): RoutineViewModelFactory {
     val application = (LocalContext.current.applicationContext as MyApplication)
     val sessionManager = application.sessionManager
