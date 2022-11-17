6998f88fd2fc8103a92be06033e2ac57a12bc606
Alejo Flores Lucey <alejo@misterflores.com>
2022-11-17 15:23:56 -0300

    Explore, Home and Routine view support different orientation and screen sizes

---
+++ app/src/main/java/com/staminapp/util/WindowInfo.kt
@@ -0,0 +1,50 @@
+package com.staminapp.util
+
+import android.content.res.Configuration
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun rememberWindowInfo(): WindowInfo {
+    val configuration = LocalConfiguration.current
+    return WindowInfo(
+        screenWidthInfo = when {
+            configuration.screenWidthDp < 600 -> WindowInfo.WindowType.Compact
+            configuration.screenWidthDp < 840 -> WindowInfo.WindowType.Medium
+            else -> WindowInfo.WindowType.Expanded
+        },
+        screenHeightInfo = when {
+            configuration.screenHeightDp < 480 -> WindowInfo.WindowType.Compact
+            configuration.screenHeightDp < 900 -> WindowInfo.WindowType.Medium
+            else -> WindowInfo.WindowType.Expanded
+        },
+        orientation = when (configuration.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> WindowInfo.Orientation.Portrait
+            Configuration.ORIENTATION_LANDSCAPE -> WindowInfo.Orientation.Landscape
+            else -> WindowInfo.Orientation.Undefined
+        },
+        screenWidth = configuration.screenWidthDp.dp,
+        screenHeight = configuration.screenHeightDp.dp
+    )
+}
+
+data class WindowInfo(
+    val screenWidthInfo: WindowType,
+    val screenHeightInfo: WindowType,
+    val orientation: Orientation,
+    val screenWidth: Dp,
+    val screenHeight: Dp
+) {
+    sealed class WindowType {
+        object Compact: WindowType()
+        object Medium: WindowType()
+        object Expanded: WindowType()
+    }
+    sealed class Orientation {
+        object Portrait: Orientation()
+        object Landscape: Orientation()
+        object Undefined: Orientation()
+    }
+}
