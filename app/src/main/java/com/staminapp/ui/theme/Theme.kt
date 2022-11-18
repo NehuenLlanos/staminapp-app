bd6d782b0e43a08aa1b6b772c39f1c2600f10333
Alejo Flores Lucey <alejo@misterflores.com>
2022-11-18 12:58:02 -0300

    Fixed dark mode problem.

--- app/src/main/java/com/staminapp/ui/theme/Theme.kt
+++ app/src/main/java/com/staminapp/ui/theme/Theme.kt
@@ -1,21 +1,9 @@
 package com.staminapp.ui.theme
 
-import android.graphics.Color
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color.Companion.White
-
-private val DarkColorPalette = darkColors(
-    primary = Primary,
-    primaryVariant = Text,
-    secondary = Secondary,
-    error = Error,
-    background = Background,
-    onBackground = Background,
-)
 
 private val LightColorPalette = lightColors(
     primary = Primary,
@@ -37,12 +25,6 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun StaminappAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
     MaterialTheme(
         colors = LightColorPalette,
         typography = Typography,
